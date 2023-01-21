4dceb7676647d0d91d68b27c59818d140ee260cd
Victoria Magda <119420321+MagdaVic@users.noreply.github.com>
2023-01-21 21:06:34 +0200

    Create 8 module, implemeted 1 and 2 tasks

---
+++ 8_module/8module_2.py
@@ -0,0 +1,23 @@
+from datetime import date
+
+
+def get_days_in_month(month, year):
+    number_days=28
+    number_days=27
+    while True:
+        try:
+            number_days=number_days+1
+            date(year,month,number_days)
+        except ValueError:
+            break
+        else:
+            max_days=number_days
+    
+
+    return max_days
+
+
+
+month=2
+year=2020
+print(get_days_in_month(month, year))
