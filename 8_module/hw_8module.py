f21709dab4c201f14b48e7a093dec8198da1d108
Victoria Magda <119420321+MagdaVic@users.noreply.github.com>
2023-01-23 16:33:19 +0200

    Finished hw from module 8

--- 8_module/hw_8module.py
+++ 8_module/hw_8module.py
@@ -1,27 +1,42 @@
 from datetime import date, datetime, timedelta
 import sys
 
-def get_birthdays_per_week(users):
+def get_dict_week_names(users):
     dict_week_days={'Monday':[0,5,6], 'Tuesday':[1], 'Wednesday':[2], 'Thursday':[3],'Friday':[4]}
     dict_week_names={'Monday':[], 'Tuesday':[], 'Wednesday':[], 'Thursday':[],'Friday':[]}
-    # if sys.argv[1]=='print':
     current_date = datetime.now().date()
-    # list_difference_days =[timedelta(days=i) for i in range(7)]
-    list_res_days=[]
     for i in users:
-        difference_days=int((i['birthday'].date()-current_date).days)
-        if (current_date.weekday()==0 and -2<=difference_days<=4): #or (current_date.weekday>0 and 0<=difference_days<=6):
+        difference_days=(i['birthday'].date()-current_date).days
+        if (current_date.weekday()==0 and -2<=difference_days<=4) or (current_date.weekday()>0 and 0<=difference_days<=6):
             for k,v in dict_week_days.items():
                 if i['birthday'].weekday() in v:
                     dict_week_names[k].append(i['name'])
-        
-        
+       
     return dict_week_names
 
-   
+def validate_correct_path():
+    try:
+        sys.argv[1]=='print'
+    except IndexError:
+        sys.exit("Write word 'print'")
+    if sys.argv[1]!='print':
+        sys.exit("Write word 'print'")
 
 
-users=[{'name':'Bob','birthday':datetime(year=2023, month=1, day=21, hour=14)},{'name':'Aob','birthday':datetime(year=2023, month=1, day=20, hour=14)},{'name':'Oob','birthday':datetime(year=2023, month=1, day=23, hour=14)},{'name':'EEb','birthday':datetime(year=2023, month=1, day=24, hour=14)},{'name':'GGob','birthday':datetime(year=2023, month=1, day=24, hour=14)},{'name':'FDFb','birthday':datetime(year=2023, month=1, day=27, hour=14)},{'name':'Bab','birthday':datetime(year=2023, month=1, day=28, hour=14)}, {'name':'Bnb','birthday':datetime(year=2023, month=1, day=30, hour=14)}]
+def print_out_in_console(dict_week_names):
+    for k,v in dict_week_names.items():
+            print(f"{k}: {', '.join(v)}")
 
-print(get_birthdays_per_week(users))
-# get_birthdays_per_week(users)
+def get_birthdays_per_week():
+    validate_correct_path()
+    dict_week_names=get_dict_week_names(users)
+    print_out_in_console(dict_week_names)
+
+
+
+if __name__ == '__main__':
+    users=[{'name':'1','birthday':datetime(year=2023, month=1, day=21, hour=14)},{'name':'2','birthday':datetime(year=2023, month=1, day=20, hour=14)},{'name':'3','birthday':datetime(year=2023, month=1, day=23, hour=14)},{'name':'4','birthday':datetime(year=2023, month=1, day=24, hour=14)},{'name':'5','birthday':datetime(year=2023, month=1, day=24, hour=14)},{'name':'6','birthday':datetime(year=2023, month=1, day=27, hour=14)},{'name':'7','birthday':datetime(year=2023, month=1, day=28, hour=14)}, {'name':'8','birthday':datetime(year=2023, month=1, day=30, hour=14)}]
+    get_birthdays_per_week()
+
+
+
