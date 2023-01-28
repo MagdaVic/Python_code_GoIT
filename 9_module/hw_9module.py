cc162b6e5a72dfd8b4b84509cd09483493acef07
Victoria Magda <119420321+MagdaVic@users.noreply.github.com>
2023-01-28 20:03:20 +0300

    Update hw from 9 module: create def hello, def add_name_phone, add change_phone

---
+++ 9_module/hw_9module.py
@@ -0,0 +1,33 @@
+import sys
+# x1 - first string after command
+# x2 - second string after command
+
+
+def hello(x1=None, x2=None):
+    print("How can I help you?")
+
+
+def add_name_phone(x1, x2):
+    if {'name': x1.lower(), 'phone': x2.lower()} not in list_name_phone:
+        list_name_phone.append({'name': x1, 'phone': x2})
+        print('New contacts are added')
+
+
+def change_phone(x1, x2):
+    for i in list_name_phone:
+        if x1 == i['name']:
+            i['phone'] = x2
+    print('New phone is changed')
+
+
+def main():
+    COMMANDS = {'hello': hello, 'add': add_name_phone, 'change': change_phone}
+
+    result_comand = COMMANDS[sys.argv[1]]
+    result_comand(sys.argv[2] if len(sys.argv) >= 3 else None,
+                  sys.argv[3] if len(sys.argv) >= 4 else None)
+
+
+if __name__ == '__main__':
+    list_name_phone = []
+    main()
