a58dea2c6a135ab3081dce031d77e39e50aeef47
Victoria Magda <119420321+MagdaVic@users.noreply.github.com>
2023-01-28 23:31:08 +0300

    Add assert isinstance  str  in functions from hw of 9 module

--- 9_module/hw_9module.py
+++ 9_module/hw_9module.py
@@ -1,31 +1,53 @@
-import sys
-# x1 - first string after command
-# x2 - second string after command
+# x1 - command string
+# x2 - first string after command (name)
+# x3 - second string after command (phone)
 
 
-def hello(x1=None, x2=None):
+def hello(x2=None, x3=None):
     print("How can I help you?")
 
 
-def add_name_phone(x1, x2):
-    if {'name': x1.lower(), 'phone': x2.lower()} not in list_name_phone:
-        list_name_phone.append({'name': x1, 'phone': x2})
-        print(f'New contacts (name: {x1}, phone: {x2}) are added')
+def add_name_phone(x2, x3):
+    assert isinstance(x2, str)
+    assert isinstance(x3, str)
+    if {'name': x2, 'phone': x3} not in list_name_phone:
+        list_name_phone.append({'name': x2, 'phone': x3})
+        print(f'New contacts (name: {x2}, phone: {x3}) are added')
 
 
-def change_phone(x1, x2):
+def change_phone(x2, x3):
+    assert isinstance(x2, str)
+    assert isinstance(x3, str)
     for i in list_name_phone:
-        if x1 == i['name']:
-            i['phone'] = x2
-    print(f'New phone of {x1} is changed')
+        if x2 == i['name']:
+            i['phone'] = x3
+    print(f'New phone of {x2} is changed')
+
+
+def phone(x2, x3=None):
+    assert isinstance(x2, str)
+    for i in list_name_phone:
+        if x2 == i['name']:
+            print(f"Phone of {x2} is {i['phone']}")
 
 
 def main():
-    COMMANDS = {'hello': hello, 'add': add_name_phone, 'change': change_phone}
-
-    result_comand = COMMANDS[sys.argv[1]]
-    result_comand(sys.argv[2] if len(sys.argv) >= 3 else None,
-                  sys.argv[3] if len(sys.argv) >= 4 else None)
+    COMMANDS = {'hello': hello, 'add': add_name_phone,
+                'change': change_phone, 'phone': phone}
+    flag = True
+    while flag:
+        commands_list = input('Enter your command:').split()
+        try:
+            commands_list[0]
+        except IndexError:
+            commands_list = input('Enter your command:').split()
+        else:
+            if commands_list[0] == 'exit':
+                flag = False
+            result_comand = COMMANDS[commands_list[0].lower()]
+            result_comand(commands_list[1] if len(commands_list) >= 2 else None,
+                          commands_list[2] if len(commands_list) >= 3 else None)
+            print(list_name_phone)
 
 
 if __name__ == '__main__':
