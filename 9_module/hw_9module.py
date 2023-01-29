b49315b4498fa5791e040ce893acc8d33fefb4a4
Victoria Magda <119420321+MagdaVic@users.noreply.github.com>
2023-01-29 17:17:35 +0300

    Create new hw from 9 modul, rename 2 files

--- 9_module/hw_9module.py
+++ 9_module/hw_9module.py
@@ -3,35 +3,33 @@
 # x3 - second string after command (phone)
 
 
-def hello(x2=None, x3=None):
+def hello(output_list):
     print("How can I help you?")
 
 
-def add_name_phone(x2, x3):
-    assert isinstance(x2, str)
-    assert isinstance(x3, str)
+def add_name_phone(output_list):
+    x2, x3, *x0 = output_list
     if {'name': x2, 'phone': x3} not in list_name_phone:
         list_name_phone.append({'name': x2, 'phone': x3})
         print(f'New contacts (name: {x2}, phone: {x3}) are added')
 
 
-def change_phone(x2, x3):
-    assert isinstance(x2, str)
-    assert isinstance(x3, str)
+def change_phone(output_list):
+    x2, x3, *x0 = output_list
     for i in list_name_phone:
         if x2 == i['name']:
             i['phone'] = x3
     print(f'New phone of {x2} is changed')
 
 
-def phone(x2, x3=None):
-    assert isinstance(x2, str)
-    for i in list_name_phone
-    if x2 == i['name']:
-        list_name_phone: print(f"Phone of {x2} is {i['phone']}")
+def phone(output_list):
+    x2, *x0 = output_list
+    for i in list_name_phone:
+        if x2 == i['name']:
+            print(f"Phone of {x2} is {i['phone']}")
 
 
-def show_all(x2=None, x3=None):
+def show_all(output_list):
     for i in list_name_phone:
         print(f"name: {i['name']}, phone: {i['phone']}")
 
@@ -41,17 +39,19 @@ def main():
                 'change': change_phone, 'phone': phone, 'show all': show_all}
     flag = True
     while flag:
-        commands_list = input('Enter your command:').split()
-        try:
-            commands_list[0]
-        except IndexError:
-            commands_list = input('Enter your command:').split()
-        else:
-            if commands_list[0] == 'exit':
-                flag = False
-            result_comand = COMMANDS[commands_list[0].lower()]
-            result_comand(commands_list[1] if len(commands_list) >= 2 else None,
-                          commands_list[2] if len(commands_list) >= 3 else None)
+        commands_string = input('Enter your command:')
+        flag_serching_command = False
+        for i in COMMANDS.keys():
+            if commands_string.lower().find(i) == 0:
+                command = commands_string[:len(i)].lower()
+                list_command_name_phone = commands_string[len(i)+1:].split()
+                flag_serching_command = True
+                break
+        if flag_serching_command == False:
+            continue
+        func_handler = COMMANDS[command]
+        func_handler(list_command_name_phone)
+        print(list_name_phone)
 
 
 if __name__ == '__main__':
