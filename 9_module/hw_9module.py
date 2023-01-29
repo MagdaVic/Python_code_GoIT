284f1e824a88282a4adf2dc270a5071dac4770c5
Victoria Magda <119420321+MagdaVic@users.noreply.github.com>
2023-01-29 18:48:17 +0300

    Add function exit_from_chat

--- 9_module/hw_9module.py
+++ 9_module/hw_9module.py
@@ -1,3 +1,4 @@
+import sys
 # x1 - command string
 # x2 - first string after command (name)
 # x3 - second string after command (phone)
@@ -34,12 +35,16 @@ def show_all(output_list):
         print(f"name: {i['name']}, phone: {i['phone']}")
 
 
+def exit_from_chat(output_list):
+    sys.exit('Good bye!')
+
+
 def main():
     COMMANDS = {'hello': hello, 'add': add_name_phone,
-                'change': change_phone, 'phone': phone, 'show all': show_all}
+                'change': change_phone, 'phone': phone, 'show all': show_all, 'good bye': exit_from_chat, 'close': exit_from_chat, 'exit': exit_from_chat}
     while True:
         commands_string = input(
-            'Enter your command (hello, add, change, phone, show all):').lstrip()
+            'Enter your command (hello, add, change, phone, show all, good bye, close, exit):').lstrip()
         for i in COMMANDS.keys():
             if commands_string.lower().startswith(i):
                 command = commands_string[:len(i)].lower()
