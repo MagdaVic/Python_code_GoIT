2facfcd207b9bd45877c8e8fbea2540398022626
Victoria Magda <119420321+MagdaVic@users.noreply.github.com>
2023-02-04 22:51:25 +0300

    Update hw from 10 module: add classes Field, Name, Phone, Record

--- 10_module/hw_10module.py
+++ 10_module/hw_10module.py
@@ -1,3 +1,6 @@
+from collections import UserDict
+
+
 class Field:
     def __init__(self, value):
         self.value = value
@@ -14,5 +17,43 @@ class Phone(Field):
 class Record:
     def __init__(self, name, phone):
         self.name = name
-        # self.phone = phone
         self.phones = []
+        if phone:
+            self.phones.append(phone)
+
+    def add_phones(self, phone):
+        if phone not in self.phones:
+            self.phones.append(phone)
+
+    def change_phones(self, phone, phone_new):
+        for count, ele in enumerate(self.phones):
+            if ele == phone:
+                self.phones[count] = phone_new
+                break
+
+    def remove_phones(self, phone):
+        for count, ele in enumerate(self.phones):
+            if ele == phone:
+                self.phones.remove(ele)
+                break
+
+    def list_phones(self):
+        return self.phones
+
+
+name1 = Name('Bob')
+print(name1.value)
+phone1 = Phone('234637373')
+print(phone1.value)
+phone2 = Phone('3647474')
+phone3 = Phone('364747555')
+phone4 = Phone('1')
+record1 = Record(name1, phone1)
+print(record1.name)
+record1.add_phones(phone2)
+record1.add_phones(phone3)
+print(record1.list_phones())
+record1.change_phones(phone1, phone4)
+print(record1.list_phones())
+record1.remove_phones(phone4)
+print(record1.list_phones())
