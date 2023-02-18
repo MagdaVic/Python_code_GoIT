88bc7a899d2fb1412e2706110d26efa3364e8395
Victoria Magda <119420321+MagdaVic@users.noreply.github.com>
2023-02-18 21:32:01 +0300

    Create 4th task from 12 module

---
+++ 12_module/12module_4.py
@@ -0,0 +1,53 @@
+import pickle
+
+
+class Person:
+    def __init__(self, name: str, email: str, phone: str, favorite: bool):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.favorite = favorite
+
+
+class Contacts:
+    def __init__(self, filename: str, contacts: list[Person] = None):
+        if contacts is None:
+            contacts = []
+        self.filename = filename
+        self.contacts = contacts
+
+    def save_to_file(self):
+        with open(filename, 'wb') as fh:
+            pickle.dump(self, fh)
+
+    def read_from_file(self):
+        with open(filename, 'rb') as fh:
+            self_unpack = pickle.load(fh)
+            return self_unpack
+
+
+contacts = [
+    Person(
+        "Allen Raymond",
+        "nulla.ante@vestibul.co.uk",
+        "(992) 914-3792",
+        False,
+    ),
+    Person(
+        "Chaim Lewis",
+        "dui.in@egetlacus.ca",
+        "(294) 840-6685",
+        False,
+    ),
+]
+filename = r'12_module\user_class.txt'
+persons = Contacts(filename, contacts)
+print(persons)
+print(persons.contacts)
+persons.save_to_file()
+person_from_file = persons.read_from_file()
+print(persons == person_from_file)  # False
+print(persons.contacts[0] == person_from_file.contacts[0])  # False
+print(persons.contacts[0].name == person_from_file.contacts[0].name)  # True
+print(persons.contacts[0].email == person_from_file.contacts[0].email)  # True
+print(persons.contacts[0].phone == person_from_file.contacts[0].phone)  # True
